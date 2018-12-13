49f3c9dae452931af755c4d2e9a7dbb87b616a68
Benjamin Rafetto <brafetto@g.harvard.edu>
2018-12-13 12:05:52 -0500

    First stab at A star. Not working yet.

---
+++ code/util.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# Currently uses average word distances from word2vec embeddings
+def get_distance(topic, model, goal, option="combined"):
+    # combined, min, and average
+    assert type(topic) is list and type(goal) is list
+    try:
+        distances = [model.distance(x, y) for x in topic for y in goal]
+        if option == "combined":
+            return (np.average(distances) + np.min(distances) / 2.0)  # Combination of average and minimum
+        elif option == "average":
+            return np.average(distances)
+        elif option == "minimum":
+            return np.min(distances)
+        else:
+            raise Exception("Invalid distance metric: {}".format(option))
+    except:
+        return np.Infinity
+
+
+def process_word(topic, model, combine_phrases=True):
+    if not topic:  #No topic. Stop.
+        return []
+    
+    if topic in model:
+        return [topic]
+
+    output = []
+    words = topic.split(' ')
+
+    if combine_phrases:
+        #Iteratively search word2vec for shorter and shorter phrases
+        for j in range(len(words), 1, -1):
+            test = '_'.join(words[:j])
+            if test in model:
+                return [test] + process_word(' '.join(words[j:]), model, combine_phrases)
+
+    if words[0] in model:
+        output.append(words[0])
+    return output + process_word(' '.join(words[1:]), model, combine_phrases)
